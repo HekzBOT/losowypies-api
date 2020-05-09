3f1e364a3b5d494606dd7968e5960560da34eeb1
Pawelikz <49805256+MliteFX@users.noreply.github.com>
2020-05-09 16:58:52 +0200

    Add files via upload

---
+++ losowypies.php
@@ -0,0 +1,16 @@
+<?php
+$plik = "123.txt"; //tutaj dajecie plik txt 
+$plik_array = file($plik); // liczba linijek z pliku
+$liczba_linijek = count($plik_array); // \/
+$ost_lin = $liczba_linijek - 1; // odejmowanie -1 od linijek z pliku ( php czyta plik od 1 linijki)
+$losowa_linijka = rand(0, $ost_lin); // losowanie losowej linijki
+$losowypies = $plik_array[$losowa_linijka]; // wylosowa liczba z array bedzie tekstem z pliku
+
+
+
+
+    $losowypies = [ 'pies' => $losowypies ]; // mozecie sobie zmienic 'pies' na np. 'los' WAŻNE ZEBY BYLO W '
+
+header('Content-type: application/json');
+echo json_encode( $losowypies )
+?>
